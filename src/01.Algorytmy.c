95f934cb98f892ed6bdd2975f5635efeccb07a7c
Lokkus <lokkus7777@gmail.com>
2016-07-17 18:12:04 +0200

    next commit

--- src/01.Algorytmy.c
+++ src/01.Algorytmy.c
@@ -10,10 +10,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+void odw_string(char *tab){
+	char *beg = tab;
+	while(*(++tab)!='\0');
+	while(*(tab--)!=*beg){
+		printf("%c",*tab);
+	}
+}
+int fibb_rek(int wyraz){
+	if(wyraz<3)
+		return 1;
+	else
+		return (fibb_rek(wyraz-2)+fibb_rek(wyraz-1));
 
+}
+
+void fibb_nor(int ile){
+	int a=1;
+	int b=0;
+
+	while(ile--){
+		a=a+b;
+		b=a-b;
+		printf("%d, ", a);
+	}
+}
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	puts ("AAAAA");
-	puts ("BBBBB");
+
+	//for(int i = 1; i<=10; i++){
+	//	printf("%d, ",fibb_rek(i));
+	//}
+	fibb_nor(10);
+
+
 	return EXIT_SUCCESS;
 }
